f30e4ceb5fa4020f0c4b493b91d20aaaf704b300
hyx0329 <hyx0329@gmail.com>
2025-01-31 15:08:23 +0800

    fix stub download script

--- download_stubs.sh
+++ download_stubs.sh
@@ -3,14 +3,14 @@
 CPY_BUNDLE_DATE=20241127
 CPY_BUNDLE_COMMUNITY_DATE=20241110
 CPY_BUNDLE=https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/download/${CPY_BUNDLE_DATE}/adafruit-circuitpython-bundle-py-${CPY_BUNDLE_DATE}.zip
-CPY_BUNDLE_COMMUNITY=https://github.com/adafruit/CircuitPython_Community_Bundle/releases/download/${20241110}/circuitpython-community-bundle-py-${20241110}.zip
+CPY_BUNDLE_COMMUNITY=https://github.com/adafruit/CircuitPython_Community_Bundle/releases/download/${CPY_BUNDLE_COMMUNITY_DATE}/circuitpython-community-bundle-py-${CPY_BUNDLE_COMMUNITY_DATE}.zip
 
 mkdir stubs
 cd stubs
 
 
 curl -fsSL -o cpy-bundle-py.zip "${CPY_BUNDLE}"
-curl -fsSL -o cpy-bundle-community-py.zip "${CPY_BUNDLE}"
+curl -fsSL -o cpy-bundle-community-py.zip "${CPY_BUNDLE_COMMUNITY}"
 
 unzip cpy-bundle-py.zip
 unzip cpy-bundle-community-py.zip
